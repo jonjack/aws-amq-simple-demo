69668e557f4e952110f54a7570b3952d45690082
Jonathan Jackson <jon.jackson@elasticpath.com>
2021-08-07 01:21:53 +0100

    Added simple consumer demo

--- src/main/java/amq/demo/App.java
+++ src/main/java/amq/demo/App.java
@@ -4,6 +4,8 @@ import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -18,20 +20,33 @@ public class App {
 	static Connection connection;
 	static Session session;
 
-	final static String mqUser = "xxxxxxxxx";
-	final static String mqPassword = "xxxxxxxx";
-	final static String mqEndpoint = "ssl://b-f30b40f1-700b-45da-a895-xxxxxxxxxxxxx-1.mq.eu-west-2.amazonaws.com:61617";
+	final static String mqUser = "xxxx";
+	final static String mqPassword = "xxxxxxx";
+	final static String mqEndpoint = "ssl://b-f30b40f1-700b-45da-a895-xxxxxxxx-1.mq.eu-west-2.amazonaws.com:61617";
 	final static String demoQueue = "demo-queue";
 	final static String demoTopic = "demo-topic";
 
 	public static void main(String[] args) throws JMSException {
-		createSession();
+		createProducerSession();
 		sendMessageToQueue("Hello ActiveMQ Queue", demoQueue);
 		sendMessageToTopic("Hello ActiveMQ Topic", demoTopic);
+		endSession();
+
+		createConsumerSession();
+		receiveMessageFromQueue(demoQueue);
 		endSession();
 	}
 
 	private static void createSession() throws JMSException {
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+	}
+
+	private static void endSession() throws JMSException {
+		session.close();
+		connection.close();
+	}
+
+	private static void createProducerSession() throws JMSException {
 		connectionFactory = new ActiveMQConnectionFactory(mqEndpoint);
 		connectionFactory.setUserName(mqUser);
 		connectionFactory.setPassword(mqPassword);
@@ -40,7 +55,16 @@ public class App {
 		pooledConnectionFactory.setMaxConnections(10);
 		connection = pooledConnectionFactory.createConnection();
 		connection.start();
-		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		createSession();
+	}
+
+	private static void createConsumerSession() throws JMSException {
+		connectionFactory = new ActiveMQConnectionFactory(mqEndpoint);
+		connectionFactory.setUserName(mqUser);
+		connectionFactory.setPassword(mqPassword);
+		connection = connectionFactory.createConnection();
+		connection.start();
+		createSession();
 	}
 
 	private static void sendMessageToQueue(String message, String queue) throws JMSException {
@@ -58,8 +82,8 @@ public class App {
 	private static void sendMessageToTopic(String message, String topic) throws JMSException {
 		// Get a reference to the target queue.
 		// Note this does not actually create a queue, the method is somewhat misleading.
-		final Destination producerDestination = session.createTopic(topic);
-		final MessageProducer producer = session.createProducer(producerDestination);
+		final Destination destination = session.createTopic(topic);
+		final MessageProducer producer = session.createProducer(destination);
 		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 		final TextMessage producerMessage = session.createTextMessage(message);
 		producer.send(producerMessage);
@@ -67,9 +91,14 @@ public class App {
 		producer.close();
 	}
 
-	private static void endSession() throws JMSException {
-		session.close();
-		connection.close();
+	private static void receiveMessageFromQueue(String queue) throws JMSException {
+		// Get a reference to the target queue.
+		final Destination destination = session.createQueue(queue);
+		final MessageConsumer consumer = session.createConsumer(destination);
+		final Message consumerMessage = consumer. receive(1000);
+		final TextMessage consumerTextMessage = (TextMessage) consumerMessage;
+		System.out.println("Message received: " + consumerTextMessage.getText());
+		consumer.close();
 	}
 
 }
